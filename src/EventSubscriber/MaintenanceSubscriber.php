e7cb607c99d2f06ceb031173901e393ddf48e1f3
ivelter <ivelter0@gmail.com>
2024-09-17 09:01:11 +0200

    add: mode maintenance (à tester)

---
+++ src/EventSubscriber/MaintenanceSubscriber.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Controller\MaintenanceController;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+
+class MaintenanceSubscriber
+{
+    public function __construct(
+        private ParameterBagInterface $parameterBag,
+        private MaintenanceController $maintenanceController
+    ) {}
+
+    #[AsEventListener]
+    public function onKernelRequest(RequestEvent $event): void {
+        $isMaintenanceActive = $this->parameterBag->get('mode_maintenance');
+        dump($isMaintenanceActive);
+        if (!$isMaintenanceActive) return;
+
+        $event->setResponse($this->maintenanceController->maintenanceRedirect());
+        $event->stopPropagation();
+    }
+}
